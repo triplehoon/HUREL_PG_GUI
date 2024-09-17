afdfbf135d4fbd0c6b2a27558d2e1c969bfcddfc
PG <PG@LAPTOP-C7R7BCNR>
2024-09-17 16:59:34 +0900

    Write Session codes

--- PG/Session.cs
+++ PG/Session.cs
@@ -1,6 +1,7 @@
 ﻿using PG.Fpga;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Reflection.Emit;
 using System.Text;
@@ -10,7 +11,12 @@ namespace PG
 {
     public class Session
     {
-        public enum SessionStatus
+        public enum eSessionType
+        {
+            NCC,
+            SMC
+        }
+        public enum eSessionStatus
         {
             Ready,
             Configuring,
@@ -20,8 +26,9 @@ namespace PG
             Stopped,
             Completed
         }
+        public eSessionType SessionType { get; private set; }
         public List<string> SessionLog { get; private set; }
-
+        const string logFileName = "log.txt";
         public string SessionMessage { 
             get
             {
@@ -32,13 +39,29 @@ namespace PG
                 string time = DateTime.Now.ToString("[yyyy-MM-dd HH:mm:ss]");
                 string log = SessionId + " " + time + " " + value;
                 SessionLog.Add(log);
+                Trace.WriteLine(log);
+                // write to log file
+                StreamWriter sw = new StreamWriter(SessionId + "/" + logFileName, true);
+                sw.WriteLine(log);
+                sw.Close();
             }
         }
 
-        public SessionStatus Status { get; private set; }
+        private eSessionStatus _status;
+        public eSessionStatus Status
+        {
+            get
+            {
+                return _status;
+            }
+            private set
+            {
+                SessionMessage = "Status changed from " + _status.ToString() + " to " + value.ToString();
+                _status = value;
+            }
+        }
 
         public string SessionId { get; private set; }
-        public string SessionName { get; private set; }
         public string SessionDescription { get; private set; }
         public DateTime SessionStartTime { get; private set; }
         public DateTime? SessionEndTime { get; private set; }
@@ -46,7 +69,7 @@ namespace PG
 
         public List<DaqData> FpgaRawData { get; private set; }
 
-        public Session(string patientId = "", string sessionDescription = "")
+        public Session(eSessionType sessionType, string patientId = "", string sessionDescription = "")
         {
             if (patientId == "")
             {
@@ -59,13 +82,55 @@ namespace PG
             PatientId = patientId;
             FpgaRawData = new List<DaqData>();
             SessionLog = new List<string>();
+            // create folder with SessionId
+            Directory.CreateDirectory(SessionId);
+            // create log file
+            File.CreateText(SessionId + "/" + logFileName);
+            SessionType = sessionType;
             SessionMessage = "Session Created";
-            Status = SessionStatus.Ready;
-        }
-        public void StartSession()
-        {
-            // Start FPGA
+            SessionMessage = "Session Type: " + SessionType.ToString();
+            Status = eSessionStatus.Ready;
         }
 
+        public void StartSession()
+        {
+            
+        }
+        private void UpdateSessionInfoToDb()
+        {
+            // update session info to db
+        }
+        private bool RunMatlabAnalysis()
+        {
+            return true;
+        }
+
+        private bool ConnectNccFtpServer()
+        {
+
+            return true;
+        }
+        private bool RunFpgaDaq()
+        {
+            return true;
+        }
+
+        private async Task UpdateFpgaToDb()
+        {
+            // update fpga data to db
+        }
+
+        private async Task UpdateNccToDb()
+        {
+            // update ncc data to db
+        }
+        
+        public void StopSession()
+        {
+            SessionMessage = "Session Stopped";
+            SessionEndTime = DateTime.Now;
+            Status = eSessionStatus.Stopped;
+            UpdateSessionInfoToDb();
+        }
     }
 }
