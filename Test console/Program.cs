09449115b16646f3b6215ef0a034c0d4d73f0ca3
PG <PG@LAPTOP-C7R7BCNR>
2024-09-17 16:13:19 +0900

    test visual updated

--- Test console/Program.cs
+++ Test console/Program.cs
@@ -19,10 +19,37 @@ class TestClass
     private static void TestFpgaDaq()
     {
         CruxellWrapper.StartFpgaDaq();
-        // wait for 30 seconds
-        System.Threading.Thread.Sleep(30000);
+        // wait for 30 seconds for every 100 ms
+        Console.WriteLine("Start reading data from FPGA");
+        for (int i = 0; i < 300; i++)
+        {
+            
+            if (CruxellWrapper.GetDataCount() != 0)
+            {
+                // comma separated values                
+                Console.WriteLine("Data count: " + CruxellWrapper.GetDataCount().ToString("N0"));
+                Console.WriteLine("Sample data: ");
+                DaqData daqData = CruxellWrapper.GetDaqData()[CruxellWrapper.GetDataCount() - 1];
+                Console.WriteLine("secTime: " + daqData.secTime.ToString("N0"));
+                Console.WriteLine("chNumber: " + daqData.chNumber.ToString("N0"));
+                Console.WriteLine("preData: " + daqData.preData.ToString("N0"));
+                Console.WriteLine("vPulseData: " + daqData.vPulseData.ToString("N0"));
+                Console.WriteLine("tPulseTime: " + daqData.tPulseTime.ToString("N0"));
+                Console.WriteLine("-------Save values-------");
+                Console.WriteLine("channel: " + daqData.channel.ToString("N0"));
+                Console.WriteLine("timestamp [ns]: " + daqData.timestamp.ToString("N0"));
+                Console.WriteLine("value [mV]: " + daqData.value.ToString("N0"));
+            }
+            System.Threading.Thread.Sleep(100);
+            // check console is write
+            if (Console.CursorTop > 5)
+            {
+                Console.SetCursorPosition(0, Console.CursorTop - 11);
+            }
+        }
+        // cursor to the end of the console
+        Console.SetCursorPosition(0, Console.CursorTop + 11);
         CruxellWrapper.StopFpgaDaq();
-        Console.WriteLine("Data count: " + CruxellWrapper.GetDataCount());
         CruxellWrapper.TestWriteData();
     }
     static void Main(string[] args)
